984ac950067dcee897c6f16a14382d16973d3607
leikaijun-620 <2721751706@qq.com>
2020-03-21 12:32:14 +0800

    第san次创建

--- src/main/java/com/bw/TestMain.java
+++ src/main/java/com/bw/TestMain.java
@@ -4,5 +4,6 @@ public class TestMain {
     public static void main(String[] args) {
         System.out.println("又是美好的一天");
         System.out.println("嗯嗯  好好浩");
+        System.out.println("来之中华烟");
     }
 }
