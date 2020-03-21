99438c556416e56ebc68e5e04539398df67952dc
kaijun-620 <kaijun_620@sina.com>
2020-03-21 16:26:30 +0800

    new dev number12

--- src/main/java/com/bw/test.java
+++ src/main/java/com/bw/test.java
@@ -4,5 +4,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("来一个烟灰缸");
         System.out.println("再来个打火机");
+        System.out.println("这里不能抽烟，不知道吗》》");
+        System.out.println("有防风的打火机，要吗？？");
     }
 }
